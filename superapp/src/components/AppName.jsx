1c33ae9b44c507327227f975d5cf576ac3532bfc
Kallesh <karthickbs4747@gmail.com>
2024-06-19 15:06:27 +0530

    ToDo APP Initial functionality DoneI

--- superapp/src/components/AppName.jsx
+++ superapp/src/components/AppName.jsx
@@ -3,6 +3,6 @@ import { Link } from 'react-router-dom'
 
 export default function AppName({ appname }) {
     return (
-        <h1>{appname} {location.pathname !== '/' && <><Link to="/" style={{ fontSize: "1.2rem", textDecoration: "none" }}>Home</Link> <br /></>} </h1>
+        <h1 style={{ backgroundColor: "#ffb74d", padding: "10px", borderRadius: "10px" }}>{appname} {location.pathname !== '/' && <>&nbsp;&nbsp;&nbsp;<Link to="/" style={{ fontSize: "1.2rem", textDecoration: "none", border: "1px solid black", padding: "5px", borderRadius: "5px" }}>Home</Link> <br /></>} </h1>
     )
 }
--- superapp/src/components/apps/TodoApp.jsx
+++ superapp/src/components/apps/TodoApp.jsx
@@ -1,15 +1,132 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import AppName from '../AppName'
+import { Link } from 'react-router-dom';
+
 
 export default function TodoApp() {
-    return (
-        <>
+
+    const [todo, setTodo] = useState([])
+    const [dodone, setDodone] = useState([])
+    let inputRef = useRef();
+    console.log(todo);
+
+    // todo.map((x, i) => <div key={Math.random() + ""}>{x}</div>)
+
+    function addToDo() {
+        if (inputRef.current.value !== '') {
+            setTodo([...todo, inputRef.current.value])
+        }
+    }
+
+    function deleteTodo(i) {
+        setTodo(todo.filter((x, y) => y !== i))
+    }
+
+    function taskDone(x, i) {
+        setTodo(todo.filter((x, y) => y !== i))
+        setDodone([...dodone, x])
+    }
+
+    function deleteDoDone(i) {
+        setDodone(dodone.filter((x, y) => y !== i))
+    }
+
+    return (<>
+        <div>
+            <AppName appname={"ToDo App"} />
+        </div>
+        <div style={{ margin: "0px 40%" }}>
+
             <div>
-                <AppName appname={"ToDo App"} />
+                <input type="text" ref={inputRef} style={{ padding: "5px" }}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            addToDo();
+                        }
+                    }} />
+                &nbsp;
+                <button onClick={addToDo} style={{ padding: "5px", cursor: "pointer", width: "60px" }}> Add</button>
             </div>
             <div>
-                
+                <br />
+
+                {todo.length !== 0 ?
+                    <table border={1} className='tbl1'>
+                        <thead>
+                            <tr>
+                                <th colSpan={2}>Task ToDo List</th>
+                            </tr>
+                            <tr>
+                                <th >Task Name</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {todo.map((x, i) => <tr key={Math.random() + ""}>
+                                <td style={{ backgroundColor: "#fff9c4", textAlign: "center" }} >{x}  </td>
+                                <td><Link onClick={() => taskDone(x, i)}>Done</Link> &nbsp;&nbsp;&nbsp; <Link onClick={() => deleteTodo(i)}>Delete</Link></td>
+                                {/* <td> {x}</td> */}
+                            </tr>)}
+                        </tbody>
+                    </table>
+                    :
+                    <table border={1} className='tbl1'>
+                        <thead>
+                            <tr>
+                                <th colSpan={3}>Task ToDo List</th>
+                            </tr>
+                            <tr>
+                                <th>Task Name</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr><td colSpan={3} style={{ textAlign: "center" }}>No Data Available</td></tr>
+                        </tbody>
+                    </table>}
+
+                <br />
+
+                {dodone.length !== 0 ?
+                    <table border={1} className='tbl1'>
+                        <thead>
+                            <tr>
+                                <th colSpan={3}>Task Completed List</th>
+                            </tr>
+                            <tr>
+                                <th>Task Name</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {dodone.map((x, i) => <tr key={Math.random() + ""}>
+                                <td style={{ backgroundColor: "#7bdcb5", textAlign: "center" }}>{x}  </td>
+                                <td>Completed</td>
+                                <td><Link onClick={() => deleteDoDone(i)}>Delete</Link></td>
+                            </tr>)}
+                        </tbody>
+                    </table>
+                    :
+                    <table border={1} className='tbl1'>
+                        <thead>
+                            <tr>
+                                <th colSpan={3}>Task Completed List</th>
+                            </tr>
+                            <tr>
+                                <th>Task Name</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr><td colSpan={3} style={{ textAlign: "center" }}>No Data Available</td></tr>
+                        </tbody>
+                    </table>}
             </div>
-        </>
+        </div >
+    </>
+
     )
 }
